c1d030bab408bb2e80e0b9b1ed0c6bb5ed08f949
Anirudh Pillutla <a.v.pillutla@gmail.com>
2019-05-20 20:32:31 -0400

    adding SNS publish and try/except block to lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,19 +5,29 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('portfolio.anirudhpillutla.info')
-    build_bucket = s3.Bucket('portfoliobuild.anirudhpillutla.info')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:450629874875:Portfolio_Deployments')
 
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
-    # s3.meta.client.download_file('portfoliobuild.anirudhpillutla.info', 'portfoliobuild.zip', portfolio_zip)
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('portfolio.anirudhpillutla.info')
+        build_bucket = s3.Bucket('portfoliobuild.anirudhpillutla.info')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print ("Job Done")
+
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed", Message="Portfolio Deployment Unsuccessful")
+        raise
 
     return "Inside Lambda Function"
