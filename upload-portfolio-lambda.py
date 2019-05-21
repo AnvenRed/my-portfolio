80bc9e8086125a348846fd50504de41402205f8e
Anirudh Pillutla <a.v.pillutla@gmail.com>
2019-05-20 22:34:05 -0400

    lambda function update to include codepipeline integration

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,13 +8,26 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:450629874875:Portfolio_Deployments')
 
+    location = {
+        "bucketName": 'portfoliobuild.anirudhpillutla.info',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+        print ("Building Portfolio from " + str(location))
+
+
         s3 = boto3.resource('s3')
         portfolio_bucket = s3.Bucket('portfolio.anirudhpillutla.info')
-        build_bucket = s3.Bucket('portfoliobuild.anirudhpillutla.info')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = io.BytesIO()
-        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"],portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -24,8 +37,15 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         print ("Job Done")
-
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+        else:
+            codepipeline.boto3.client('codepipeline')
+            codepipeline.put_job_failure_result(jobId=job["id"])
+
     except:
         topic.publish(Subject="Portfolio Deployment Failed", Message="Portfolio Deployment Unsuccessful")
         raise
