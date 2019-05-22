c988335bd9b637fbf8587d32089dac8c16300c95
Anirudh Pillutla <a.v.pillutla@gmail.com>
2019-05-21 20:21:28 -0400

    adding description for portfolio boilerplate bubble

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import ExampleWork from './example-work';
 const myWork = [
   {
     'title': "Portfolio Boilerplate",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc': "This is my professional portfolio website. It uses ReactJS, Babel, Jest, AWS Codebuild, AWS CodePipeline, Python in AWS Lambda, AWS S3, Git and GitHub, HTML, CSS",
     'href': "https://example.com",
     'image': {
       'desc': "A Serverless Portfolio",
